af8cc1df105ef4c9333a4c264c8b5bb025a66526
Nick Perkins <nickperkins@protonmail.com>
2016-09-16 10:12:58 -0400

    finished code challenge

--- ListOfInts-HighestProductOf3Ints/main.swift
+++ ListOfInts-HighestProductOf3Ints/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+// Question: Given a list_of_ints, find the highest_product you can get from three of the
+// integers. The input list_of_ints will always have at least three integers.
 
+var array = [1,15,2,7,43,6,2,10]
+
+var largestIntsArray: NSMutableArray = []
+var result: Int = 0
+
+array.sort()
+
+var i = 0
+
+while i <= 2 {
+    
+    var lastInt = array.removeLast()
+    largestIntsArray.add(lastInt)
+    result = result + lastInt
+    i += 1
+    
+}
+
+print(largestIntsArray)
+print(result)

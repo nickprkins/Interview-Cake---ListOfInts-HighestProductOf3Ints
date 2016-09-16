e60a2077b4b48ebf893ea0719109b38fab2ca577
Nick Perkins <nutternotbutter@cpe-2606-a000-4f8e-1c00-8a53-95ff-fe2f-f4b9.dyn6.twc.com>
2016-09-16 09:46:40 -0400

    Initial Commit

---
+++ ListOfInts-HighestProductOf3Ints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ListOfInts-HighestProductOf3Ints
+//
+//  Created by Nick Perkins on 9/16/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
